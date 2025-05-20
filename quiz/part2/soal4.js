03ae3fb70993e04e19d794398faba1ba3e399cc3
Muss24 <abdulmustopa2004@gmail.com>
2025-05-20 10:16:05 +0700

    quiz part 2

---
+++ quiz/part2/soal4.js
@@ -0,0 +1,28 @@
+// ## Soal 4. Breaking Sentence (yet Again) and Count Each Length
+// ```js
+// // Problem
+// // Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3);
+
+let firstWordLength = exampleFirstWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+
+let secondWord4 = word4.substring(4, 14);
+let secondWordLength = secondWord4.length;
+console.log('Second Word:', secondWord4 + ', width lenght:', secondWordLength);
+
+let thirdWord4 = word4.substring(15, 17);
+let thirdWordLenght = thirdWord4.length;
+console.log('Third Word:', thirdWord4 + ', width lenght:', thirdWordLenght);
+
+let fourthWord4 = word4.substring(18, 20);
+let fourthWordLenght = fourthWord4.length;
+console.log('Fourth Word:', fourthWord4 + ', width lenght:', fourthWordLenght);
+
+let fifthWord4 = word4.substring(21, 25);
+let fifthWord4Lenght = fifthWord4.length;
+console.log('Fifth WOrd:', fifthWord4 + ', width lenght:', fifthWord4Lenght);
